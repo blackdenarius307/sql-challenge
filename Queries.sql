520b65c27684fa82057186c92fbf5c855a8d5e22
Zachary <zach.gibbs@gmail.com>
2020-11-08 15:01:02 -0500

    Question 7

--- Queries.sql
+++ Queries.sql
@@ -78,3 +78,19 @@ INNER JOIN departments ON
 	dept_emp.dept_no = departments.dept_no
 WHERE
 	dept_name = 'Sales';
+--Bring out the Developers too!
+SELECT
+	employees.emp_no,
+	employees.last_name,
+	employees.first_name,
+	departments.dept_name
+FROM
+	employees
+INNER JOIN dept_emp ON
+	employees.emp_no = dept_emp.emp_no
+INNER JOIN departments ON
+	dept_emp.dept_no = departments.dept_no
+WHERE
+	dept_name = 'Sales'
+OR
+	dept_name = 'Development';
