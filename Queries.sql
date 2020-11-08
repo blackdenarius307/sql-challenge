65a3bfe96d2f84c3ab09691e9cc7e42a9054afce
Zachary <zach.gibbs@gmail.com>
2020-11-08 13:18:00 -0500

    3rd question done

--- Queries.sql
+++ Queries.sql
@@ -1,3 +1,4 @@
+-- Doing a join to pull this information for Question 1.
 SELECT 
 	employees.emp_no, 
 	employees.last_name, 
@@ -8,7 +9,8 @@ FROM
 	employees
 INNER JOIN salaries ON
 	employees.emp_no = salaries.emp_no;
-SELECT * FROM employees;
+
+--Finding all employees hired in 1986
 SELECT
 	first_name,
 	last_name,
@@ -17,3 +19,18 @@ FROM
 	employees
 WHERE
 	hire_date LIKE '%1986';
+
+-- Time to get Double Jointed
+SELECT
+	dept_manager.dept_no,
+	departments.dept_name,
+	dept_manager.emp_no,
+	employees.last_name,
+	employees.first_name
+FROM
+	dept_manager
+INNER JOIN departments ON
+	dept_manager.dept_no = departments.dept_no
+INNER JOIN employees ON
+	dept_manager.emp_no = employees.emp_no;
+	
