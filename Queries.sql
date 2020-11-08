4b139c8cf92a1c4b38548b9a8f6f91ff445cd7eb
Zachary <zach.gibbs@gmail.com>
2020-11-08 14:56:37 -0500

    Question 6

--- Queries.sql
+++ Queries.sql
@@ -63,3 +63,18 @@ AND
 	last_name
 LIKE
 	'B%';
+
+--Find the Salesmen and Saleswomen. Auditions have gone up for Death of a Salesman and they're looking for real world experience.
+SELECT
+	employees.emp_no,
+	employees.last_name,
+	employees.first_name,
+	departments.dept_name
+FROM
+	employees
+INNER JOIN dept_emp ON
+	employees.emp_no = dept_emp.emp_no
+INNER JOIN departments ON
+	dept_emp.dept_no = departments.dept_no
+WHERE
+	dept_name = 'Sales';
