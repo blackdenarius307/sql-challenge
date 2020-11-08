666ea342eefdf5c4a83ee757a5f9769395bf67d0
Zachary <zach.gibbs@gmail.com>
2020-11-08 12:49:19 -0500

    First Question: Donezo

--- Queries.sql
+++ Queries.sql
@@ -1,2 +1,10 @@
-SELECT * FROM employees;
-SELECT * FROM dept_emp;
+SELECT 
+	employees.emp_no, 
+	employees.last_name, 
+	employees.first_name, 
+	employees.sex,
+	salaries.salary
+FROM 
+	employees
+INNER JOIN salaries ON
+	employees.emp_no = salaries.emp_no;
